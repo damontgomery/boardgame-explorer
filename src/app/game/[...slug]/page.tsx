6d8ceb753b012586fa25dd8b7028a92125be1a74
Dan Montgomery <dan@montgomery.onl>
2025-03-10 17:49:42 -0500

    Update game page to show available data and catch errors.

--- src/app/game/[...slug]/page.tsx
+++ src/app/game/[...slug]/page.tsx
@@ -9,20 +9,29 @@ export default async function Game({
   // We throw away the rest of the slug and just use the ID to fetch.
   const [id] = slug
 
-  const game = await getBoardGame(id)
+  try {
+    const game = await getBoardGame(id)
 
-  const { name, description, players, boardGameGeekLink, coverImage, rulesLink, status } = game
+    const { name, description, players, boardGameGeekLink, coverImage, rulesLink, status } = game
 
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>id: {id}</p>
-      <p>description: {description}</p>
-      <p>players: {players}</p>
-      <p>boardGameGeekLink: {boardGameGeekLink}</p>
-      <p>coverImage: {coverImage}</p>
-      <p>rulesLink: {rulesLink}</p>
-      <p>status: {status}</p>
-    </div>
-  )
+    return (
+      <div>
+        <h1>{name}</h1>
+        <p>id: {id}</p>
+        <p>description: {description}</p>
+        <p>players: {players.join(', ')}</p>
+        <p>boardGameGeekLink: {boardGameGeekLink}</p>
+        <p>coverImage: {coverImage}</p>
+        <p>rulesLink: {rulesLink}</p>
+        <p>status: {status}</p>
+      </div>
+    )
+  }
+  catch {
+    return (
+      <div>
+        <h1>Game not found</h1>
+      </div>
+    )
+  }
 }

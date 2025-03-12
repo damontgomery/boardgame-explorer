aa6f39d58b6545c6c76681e27b3b5ed56bbbf456
Dan Montgomery <dan@montgomery.onl>
2025-03-11 20:24:50 -0500

    Initialize search experience with all results.

--- src/feature/GameSearchExperience.tsx
+++ src/feature/GameSearchExperience.tsx
@@ -3,10 +3,34 @@
 import GameSearchStoreProvider from "@/lib/gameSearch/feature/GameSearchStoreProvider"
 import { GameSearchListing } from "./GameSearchListing"
 import { GameSearchFilters } from "./GameSearchFilters"
+import { executeSearch, selectResults } from "@/lib/gameSearch/data-access/gameSearchSlice"
+import { useGameSearchDispatch, useGameSearchSelector } from "@/lib/gameSearch/data-access/hooks"
+import { useEffect, useState } from "react"
+
+// This needs to be in the context of the GameSearchStoreProvider.
+const InitializeGameSearch = () => {
+  const [initialLoad, setInitialLoad] = useState(true)
+  const games = useGameSearchSelector(selectResults)
+
+  const dispatch = useGameSearchDispatch()
+
+  useEffect(() => {
+    if (initialLoad && games.length === 0) {
+      setInitialLoad(false)
+    }
+
+    dispatch(executeSearch())
+  }, [dispatch, initialLoad, games.length])
+
+  return null
+}
 
 export const GameSearchExperience = () => {
+  
+
   return (
     <GameSearchStoreProvider>
+      <InitializeGameSearch />
       <GameSearchFilters />
       <GameSearchListing />
     </GameSearchStoreProvider>
